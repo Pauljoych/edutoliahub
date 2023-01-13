4f437ccd6a4efa49f8e186c90d0a7f6dcaea72ff
Pauljoych <luthfiquart@gmail.com>
2023-01-13 12:14:03 +0300

    remove social media base ui

--- site.config.ts
+++ site.config.ts
@@ -20,10 +20,10 @@ export default siteConfig({
   // social usernames (optional)
   // twitter: '',
   // github: '',
-  linkedin: 'edutolia',
+  // linkedin: 'edutolia',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: '@edutoliaeducation', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // youtube: '@edutoliaeducation', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -43,11 +43,11 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   // https://www.notion.so/pauljoych/Content-Planner-fc72ea3389f1421eb5123806e19bc5c6
-  pageUrlOverrides: {
-    '/content': 'fc72ea3389f1421eb5123806e19bc5c6',
-  },
+  // pageUrlOverrides: {
+  //   '/content': 'fc72ea3389f1421eb5123806e19bc5c6',
+  // },
 
-  // pageUrlOverrides: null,
+  pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
